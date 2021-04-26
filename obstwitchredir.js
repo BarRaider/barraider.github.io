25898ff26e3b08f704c8ff9aefcbf9d6497d64c8
BarRaider <46548278+BarRaider@users.noreply.github.com>
2021-04-26 16:02:30 -0700

    OBS Twitch redir

---
+++ obstwitchredir.js
@@ -0,0 +1,13 @@
+$().ready(function() {
+   var urlParams = document.location.hash.replace('#','').split('&');
+   urlParams.forEach(function(element) {
+      var keyvalue = element.split('=');
+      if (keyvalue.length == 2 && keyvalue[0] == 'access_token') {
+	     window.location.href = "http://localhost:4202/?access_token=" + keyvalue[1];
+		 return;
+	  }
+   });
+   setTimeout(function() { 
+		$("#approvalCode").html("Something went wrong! Try restarting the Stream Deck or head to our <a href='https://buz.bz/d'>Discord</a> for support");
+   }, 7000);
+});
---
+++ obstwitchredir.md
@@ -0,0 +1,10 @@
+---
+layout: twitchredir
+---
+
+<img src="/images/twitchbanner.png" class="logo"/>  
+<div class="clearFloat">&nbsp;</div>
+
+# Hold on... Registration in process...
+
+<span id="approvalCode"></span>
