d27bbee71d47b9743ce0564c44a66f53bd33e69c
BarRaider <46548278+BarRaider@users.noreply.github.com>
2024-12-28 23:22:14 +0200

    Update plugin details

--- pluginupdate.js
+++ pluginupdate.js
@@ -1,7 +1,7 @@
 $().ready(function() {
    var urlParams = new URLSearchParams(window.location.search);
    var pname = urlParams.get('name');
-   var asset = urlParams.get('asset');
+   var asset = "https://marketplace.elgato.com/product/" + urlParams.get('asset');
    $("#pluginName").html(pname);
-   $("#pluginStore").src = "https://marketplace.elgato.com/product/" + asset;
+   $("#pluginStore").attr('src', asset);
 });
